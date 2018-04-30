729650988dcc34d84c47fd9184f9bb1ea3fa1604
Jongu Yu <joeyyu@pal-nat186-156-253.itap.purdue.edu>
2018-04-29 23:22:09 -0400

    added magicCat.cpp

---
+++ magicCat.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+	std::cout << "마법냥이가 나타났어요!" << std::endl;
+	std::cout << "/////////cat///////\n  /\\ \n /  \\ \n.∧＿∧ \n( ･ω･｡)つ━☆・*。 \n⊂　 ノ 　　　・゜+. \nしーＪ　　　°。+ *\n/////////////////\n" << std::endl;
+	std::cout << "if 중간고사를 잘보시고 싶다면 then 10명에게 이 고양이를 보내세요!\n" << std::endl;
+}
